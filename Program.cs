a17817d8c6526b33a0a88159c2702d85d3702b52
RushikeshAvhad <avhadrushikesh1804@gmail.com>
2024-06-09 12:51:48 +0530

    Get Table Record

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ public class Program
         // createDB();     //  Create Database
         // creteTable();   //  Create Table
         // AddCustomer();  //  Add Customer
+        GetCustomer();
+        Console.ReadLine();
     }
 
     #region Create Database
@@ -96,4 +98,38 @@ public class Program
 
     #endregion
 
+    #region Get Record from table
+    public static void GetCustomer()
+    {
+        string sqlStatement = "SELECT * FROM CUSTOMER";
+        SqlConnection sqlConnection = new SqlConnection(conString);
+
+        try
+        {
+            sqlConnection.Open();
+            Console.WriteLine("Connection Open");
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            Console.WriteLine($"| {"ID",-5} | {"NAME",-20} | {"BALANCE",-10} |");
+            Console.WriteLine(new string('-', 45));
+
+            while (sqlDataReader.Read())
+            {
+                Console.WriteLine($"| {sqlDataReader.GetInt32(0),-5} | {sqlDataReader.GetString(1),-20} | {sqlDataReader.GetDecimal(2),-10} |");
+                Console.WriteLine(new string('-', 45));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            sqlConnection.Close();
+            Console.WriteLine("Connection Close");
+        }
+    }
+
+    #endregion
 }
