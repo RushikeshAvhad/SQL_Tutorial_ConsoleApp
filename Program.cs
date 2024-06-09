f23b552fcbab9b581fc328a2aa9b700c661be6fc
RushikeshAvhad <avhadrushikesh1804@gmail.com>
2024-06-09 12:17:21 +0530

    Create table

--- Program.cs
+++ Program.cs
@@ -3,12 +3,18 @@ using System.Data;
 
 public class Program
 {
+
+    const string conString = "Data Source=localhost\\SQLEXPRESS; Integrated Security=SSPI; Initial Catalog=MYTESTDB;";
+
+
     public static void Main(string[] args)
     {
-        createDB();
+        // createDB();  //  Create Database
+        creteTable();   //  Create Table
         Console.WriteLine("Database Created Successfully!!");
     }
 
+    #region Create Database
     private static void createDB()
     {
         string connectionString = "Data Source=localhost\\SQLEXPRESS; Integrated Security=SSPI; Initial Catalog=;";
@@ -36,4 +42,31 @@ public class Program
             }
         }
     }
+    #endregion
+
+    private static void creteTable()
+    {
+        SqlConnection sqlConnection = new SqlConnection(conString);
+        string sqlStatement = "CREATE TABLE Customer(ID int primary key identity, name varchar(50), Balance money)";
+        try
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+            Console.WriteLine("Connection Open");
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            Console.WriteLine("Table Created Successfully !!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            sqlConnection.Close();
+        }
+    }
+
 }
