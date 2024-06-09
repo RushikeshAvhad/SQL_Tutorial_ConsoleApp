35595e8c8b219bdb1217de4d5f3bb3240674b03d
RushikeshAvhad <avhadrushikesh1804@gmail.com>
2024-06-09 12:10:32 +0530

    Create Database

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
-﻿public class Program
-{
+﻿using System.Data.SqlClient;
+using System.Data;
 
+public class Program
+{
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
-        Console.ReadLine();
+        createDB();
+        Console.WriteLine("Database Created Successfully!!");
+    }
+
+    private static void createDB()
+    {
+        string connectionString = "Data Source=localhost\\SQLEXPRESS; Integrated Security=SSPI; Initial Catalog=;";
+        string cmdText = "CREATE DATABASE MYTESTDB";
+        SqlConnection sqlconnection = new SqlConnection(connectionString);
+
+        if (sqlconnection.State != ConnectionState.Open)
+        {
+            try
+            {
+                sqlconnection.Open();
+                Console.WriteLine("Connection Open");
+                SqlCommand sqlCommand = new SqlCommand(cmdText, sqlconnection);
+                sqlCommand.ExecuteNonQuery();
+                Console.WriteLine("Database Created");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sqlconnection.Close();
+                Console.WriteLine("Connection Closed");
+            }
+        }
     }
 }
